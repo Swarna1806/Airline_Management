a7b032ca866eefa0247a3306db7b8e0b01cbea0f
Keerthi <keerthisarikakasani@gmail.com>
2024-06-15 20:36:27 +0530

    Update FlightCard.js

--- Frontend/src/components/FlightCard/FlightCard.js
+++ Frontend/src/components/FlightCard/FlightCard.js
@@ -26,7 +26,7 @@ const FlightCard = ({ flight }) => {
       console.log(dataToSend);
 
       // POST request using Axios
-      const response = await axios.post('https://airline-2-z147.onrender.com/api/book/bookflight', dataToSend, {
+      const response = await axios.post('https://airline-4.onrender.com/api/book/bookflight', dataToSend, {
       main
         headers: {
           Authorization: token,
